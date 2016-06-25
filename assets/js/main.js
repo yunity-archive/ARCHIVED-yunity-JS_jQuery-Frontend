b3bac76a5595b993e74db8513b87616065f1eb4f
D0nPiano <spam-mails@abwesend.de>
2016-06-25 18:35:40 +0200

    Pages loaded seperatly
    Pages are now in folder "pages" and can be called via "<a>" with
    "href='#...'" and "class='jumplink'"
    
    Number of Chat-Notifications can now be set via
    updateChatNotifNumber(numberToSet)

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,10 +3,9 @@
 	html5up.net | @ajlkn
 	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
 */
-
 (function($) {
-
-	var settings = {
+    
+ 	var settings = {
 
 		// Speed to resize panel.
 			resizeSpeed: 600,
@@ -42,127 +41,119 @@
 					}
 				}
 			})
-			.on('+desktop', function() {
+			
+            var	$body = $('body'),
+                $main = $('#main'),
+                $panels = $main.find('.panel'),
+                $hbw = $('html,body,window'),
+                $footer = $('#footer'),
+                $wrapper = $('#wrapper'),
+                $nav = $('#nav'), $nav_links = $nav.find('a'),
+                $jumplinks = $('.jumplink'),
+                $form = $('form'),
+                panels = [],
+                activePanelId = null,
+                firstPanelId = 'homeScreen',
+                isLocked = false,
+                hash = window.location.hash.substring(1);
 
-				var	$body = $('body'),
-					$main = $('#main'),
-					$panels = $main.find('.panel'),
-					$hbw = $('html,body,window'),
-					$footer = $('#footer'),
-					$wrapper = $('#wrapper'),
-					$nav = $('#nav'), $nav_links = $nav.find('a'),
-					$jumplinks = $('.jumplink'),
-					$form = $('form'),
-					panels = [],
-					activePanelId = null,
-					firstPanelId = null,
-					isLocked = false,
-					hash = window.location.hash.substring(1);
+	   function loadPage(id){
+                    instant = false
+    
+            // Check lock state and determine whether we're already at the target.
+                    if (isLocked
+                    ||	activePanelId == id)
+                            return false;
 
-				if (skel.vars.touch) {
+            // Lock.
+                    isLocked = true;
 
-					settings.fadeSpeed = 0;
-					settings.resizeSpeed = 0;
-					$nav_links.find('span').remove();
+            // Change nav link (if it exists).
+                    $nav_links.removeClass('active');
+                    $nav_links.filter('[href="#' + id + '"]').addClass('active');
 
-				}
+            // Change hash.
+                    if (id == firstPanelId)
+                            window.location.hash = '#';
+                    else
+                            window.location.hash = '#' + id;
+                        		  
+                    $.ajax({
+                            url: "pages/" + id + ".html",
+                            method: 'GET',
+                            dataType: "html",
+                            success : function(data) {
+                                                            
+                            // Add bottom padding.
+                                var x = parseInt($wrapper.css('padding-top')) +
+                                                $("article").first().outerHeight() +
+                                                $nav.outerHeight() +
+                                                $footer.outerHeight();
+
+                                if (x > $window.height())
+                                        $wrapper.addClass('tall');
+                                else
+                                        $wrapper.removeClass('tall');
+                                    
+                        // Fade out active panel.
+                                $footer.fadeTo(settings.fadeSpeed, 0.0001);
+                                $("article").first().fadeOut(instant ? 0 : settings.fadeSpeed, function() {
+
+                                                 $("#main").html(data.toString());
+                                                 $("article").first().hide();
+                                                 eval($("#main script").first().innerHTML);
+                                                 
+                                        // Set new active.
+                                                activePanelId = id;
+
+                                                // Force scroll to top.
+                                                        $hbw.animate({
+                                                                scrollTop: 0
+                                                        }, settings.resizeSpeed, 'swing');
+
+                                                // Reposition.
+                                                        $body._reposition();
+                                                        
+                                                // Resize main to height of new panel.
+                                                
+                                        
+                                                        $main.animate({
+                                                                height: $("article").first().outerHeight()
+                                                        }, instant ? 0 : settings.resizeSpeed, 'swing', function() {
+                                                            
+                                                        // Fade in new active panel.
+                                                                        $footer.fadeTo(instant ? 0 : settings.fadeSpeed, 1.0);
+                                                                        $("article").first().fadeIn(instant ? 0 : settings.fadeSpeed, function() {
+
+                                                                                // Unlock.
+                                                                                        isLocked = false;
+
+                                                                        });
+                                                        });
+
+                                });
+                            }
+                    });
+
+
+}		
 
 				// Body.
 					$body._resize = function() {
 						var factor = ($window.width() * $window.height()) / (1440 * 900);
 						$body.css('font-size', Math.min(Math.max(Math.floor(factor * settings.sizeFactor), settings.sizeMin), settings.sizeMax) + 'pt');
-						$main.height(panels[activePanelId].outerHeight());
+						$main.height($("article").first().outerHeight());
 						$body._reposition();
 					};
 
 					$body._reposition = function() {
 						if (skel.vars.touch && (window.orientation == 0 || window.orientation == 180))
-							$wrapper.css('padding-top', Math.max((($window.height() - (panels[activePanelId].outerHeight() + $footer.outerHeight())) / 2) - $nav.height(), 30) + 'px');
+							$wrapper.css('padding-top', Math.max((($window.height() - ($("article").first().outerHeight() + $footer.outerHeight())) / 2) - $nav.height(), 30) + 'px');
 						else
-							$wrapper.css('padding-top', ((($window.height() - panels[firstPanelId].height()) / 2) - $nav.height()) + 'px');
+							$wrapper.css('padding-top', ((($window.height() - $("article").first().height()) / 2) - $nav.height()) + 'px');
 					};
 
-				// Panels.
-					$panels.each(function(i) {
-						var t = $(this), id = t.attr('id');
-
-						panels[id] = t;
-
-						if (i == 0) {
-
-							firstPanelId = id;
-							activePanelId = id;
-
-						}
-						else
-							t.hide();
-
-						t._activate = function(instant) {
-
-							// Check lock state and determine whether we're already at the target.
-								if (isLocked
-								||	activePanelId == id)
-									return false;
-
-							// Lock.
-								isLocked = true;
-
-							// Change nav link (if it exists).
-								$nav_links.removeClass('active');
-								$nav_links.filter('[href="#' + id + '"]').addClass('active');
-
-							// Change hash.
-								if (i == 0)
-									window.location.hash = '#';
-								else
-									window.location.hash = '#' + id;
-
-							// Add bottom padding.
-								var x = parseInt($wrapper.css('padding-top')) +
-										panels[id].outerHeight() +
-										$nav.outerHeight() +
-										$footer.outerHeight();
-
-								if (x > $window.height())
-									$wrapper.addClass('tall');
-								else
-									$wrapper.removeClass('tall');
-
-							// Fade out active panel.
-								$footer.fadeTo(settings.fadeSpeed, 0.0001);
-								panels[activePanelId].fadeOut(instant ? 0 : settings.fadeSpeed, function() {
-
-									// Set new active.
-										activePanelId = id;
-
-										// Force scroll to top.
-											$hbw.animate({
-												scrollTop: 0
-											}, settings.resizeSpeed, 'swing');
-
-										// Reposition.
-											$body._reposition();
-
-										// Resize main to height of new panel.
-											$main.animate({
-												height: panels[activePanelId].outerHeight()
-											}, instant ? 0 : settings.resizeSpeed, 'swing', function() {
-
-												// Fade in new active panel.
-													$footer.fadeTo(instant ? 0 : settings.fadeSpeed, 1.0);
-													panels[activePanelId].fadeIn(instant ? 0 : settings.fadeSpeed, function() {
-
-														// Unlock.
-															isLocked = false;
-
-													});
-											});
-
-								});
-
-						};
-
-					});
+				
 
 				// Nav + Jumplinks.
 					$nav_links.add($jumplinks).click(function(e) {
@@ -174,9 +165,7 @@
 							e.stopPropagation();
 
 							id = href.substring(1);
-
-							if (id in panels)
-								panels[id]._activate();
+							loadPage(href.substring(1))
 
 						}
 
@@ -224,25 +213,22 @@
 					$window
 						.trigger('resize');
 
-					if (hash && hash in panels)
-						panels[hash]._activate(true);
+					if (hash) {
+						loadPage(hash)
+                                            } else {
+                                            loadPage(firstPanelId)
+                                        }
 
 					$wrapper.fadeTo(400, 1.0);
 
-			})
-			.on('-desktop', function() {
-
-				window.setTimeout(function() {
-					location.reload(true);
-				}, 50);
-
-			});
 
 	});
 
 })(jQuery);
 
 // ************** Functions for ajax Requests ***********************
+
+var baseDomain = "https://dev.yunity.org";
 
 function loginUserSuccess(data, status, xhr){
 	var cookieToSet=xhr.getResponseHeader('Connection');
@@ -259,17 +245,12 @@ function loginUser(){
 	
 	var email = document.getElementById("login-email-input").value;;
 	var pw = document.getElementById("login-pw-input").value;
-	$.ajax("https://dev.yunity.org/api/auth/", {
+	$.ajax(baseDomain + "/api/auth/", {
 		 method: 'POST',
 		 data: {email: email, password: pw},
 		 crossDomain: true,
 		 success: loginUserSuccess,
-		 error: loginUserError,
-		 complete:function (xhr) {
-				var cookieToSet=xhr.getResponseHeader('Connection');
-				alert(cookieToSet);
-				console.log(xhr)
-		 }
+		 error: loginUserError
 	  });
 }
 
@@ -281,7 +262,7 @@ function signUp(){
 	var pw = document.getElementById("signUp-pw-input").value;
 	var pwRepeat = document.getElementById("signUp-pwRepeat-input").value;
 	var email = document.getElementById("signUp-email-input").value;
-	$.post("https://dev.yunity.org/api/users/",
+	$.post(baseDomain + "/api/users/",
 			{
 				display_name: username,
 				first_name: firstname,
@@ -301,7 +282,7 @@ function signUp(){
 
 function getUsers() {
 	$.ajax({
-		url: "https://dev.yunity.org/api/users/",
+		url: baseDomain + "/api/users/",
 		dataType: "json",
 		success : function(data) {
 			displayUsers(data);
@@ -310,12 +291,24 @@ function getUsers() {
 	return;
 }
 
-// https://dev.yunity.org
+/**
+ * Shows the Notification Bubble at the Chat icon
+ *
+ * @param {number} notifNumber - The number of Notifications
+ */
+function updateChatNotifNumber(notifNumber){
+    if(notifNumber == 0){
+        $("#navChat").html('<span>Chat</span>')
+    } else {
+        $("#navChat").html('<div class="circle notification">'+notifNumber+'</div><span>Chat</span>')        
+    }
+}
+
 
 
 function isLoggedIn(){				  
 	$.ajax({
-		url: "https://dev.yunity.org/api/auth/status",
+		url: baseDomain + "/api/auth/status/",
 		method: 'GET',
 		dataType: "json",
 		crossDomain: true,
@@ -328,7 +321,6 @@ function isLoggedIn(){
 			}
 		}
 	});
-	return;
 	
 }
 
@@ -338,7 +330,7 @@ function displayUsers(data){
 	data.forEach(function(entry) {
 		if(firstOne){
 			chatUserString += '<div class="chat-user active">'+entry["display_name"]+'</div>';
-			firstOne = false;						
+			firstOne = false;
 		} else {
 			chatUserString += '<div class="chat-user">'+entry["display_name"]+'</div>';						
 		}
@@ -346,8 +338,18 @@ function displayUsers(data){
 	document.getElementById("chat-users").innerHTML = chatUserString;
 }
 
-$( document ).ready(function() {
-	getUsers();
-	//isLoggedIn();
+$( "#logStatus" ).click(function() {
+    // add login / logout functionality here later
+});
+
+
+$( document ).ready(function() {   
+        //isLoggedIn();
+        updateChatNotifNumber(5)
+        $.ajaxSetup({
+            xhrFields: {
+		withCredentials: true
+            }
+        });
 });
 	  		
